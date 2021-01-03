541013bd63257395444c3544f3a674dee3c7b616
Todd Jones <jonesgtodd@gmail.com>
2021-01-03 13:55:40 -0600

    created a contactpage

--- src/pages/ContactOage.js
+++ src/pages/ContactOage.js
@@ -20,3 +20,47 @@ class ContactPage extends React.Component {
             emailSent: null,
         }
     }
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(event.target);
+
+        this.setState({
+            disabled: true
+        });
+
+        Axios.post('http://localhost:3030/api/email', this.state)
+            .then(res => {
+                if(res.data.success) {
+                    this.setState({
+                        disabled: false,
+                        emailSent: true
+                    });
+                } else {
+                    this.setState({
+                        disabled: false,
+                        emailSent: false
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+
+                this.setState({
+                    disabled: false,
+                    emailSent: false
+                });
+            })
+
+    }
