01da0468be01c08a8f77eec77f4e4d8480dcdd64
Todd Jones <jonesgtodd@gmail.com>
2021-01-03 13:46:09 -0600

    created app,js

--- src/App.js
+++ src/App.js
@@ -35,3 +35,20 @@ class App extends React.Component {
       }
     }
   }
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Todd Jones</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
