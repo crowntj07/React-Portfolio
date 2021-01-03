3002afac1999c390138d223c582f5c166d5752cd
Todd Jones <jonesgtodd@gmail.com>
2021-01-03 13:53:43 -0600

    created a Aboutpage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,3 +7,12 @@ function AboutPage(props) {
     return(
         <div>
             <Hero title={props.title} />
+            <Content>
+          <p> Hello! I am an Information Technology professional who works as a Desktop Support Tech at a local clinic. I have worked in the IT Industry for about 6 years now. I have many certifications such as CCNA, ITIL, and MCP. I am a very creative person. I have experience in programs like Adobe Photoshop, Illustrator, and InDesign. Becoming a Software Developer is my goal! I’ve always had a desire for coding, and I wanted to know how so many codes can make a beautiful website or app. Joining Vanderbilt University Boot Camp has been awesome thus far and I’m learning so much!</p> 
+            </Content>
+        </div>
+    );
+
+}
+
+export default AboutPage;
