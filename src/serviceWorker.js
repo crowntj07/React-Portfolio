e741859fa34d9aa2062ab1f44d9e9bb59aba183f
Todd Jones <jonesgtodd@gmail.com>
2021-01-03 13:36:38 -0600

    created serviceworker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,15 @@
+// This optional code is used to register a service worker.
+// register() is not called by default.
+
+// This lets the app load faster on subsequent visits in production, and gives
+// it offline capabilities. However, it also means that developers (and users)
+// will only see deployed updates on subsequent visits to a page, after all the
+// existing tabs open on the page have been closed, since previously cached
+// resources are updated in the background.
+
+// To learn more about the benefits of this model and instructions on how to
+// opt-in, read https://bit.ly/CRA-PWA
+
 const isLocalhost = Boolean(
     window.location.hostname === 'localhost' ||
       // [::1] is the IPv6 localhost address.
@@ -7,6 +19,7 @@ const isLocalhost = Boolean(
         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
       )
   );
+  
   export function register(config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
       // The URL constructor is available in all browsers that support SW.
@@ -17,6 +30,7 @@ const isLocalhost = Boolean(
         // serve assets; see https://github.com/facebook/create-react-app/issues/2374
         return;
       }
+  
       window.addEventListener('load', () => {
         const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
   
@@ -39,6 +53,7 @@ const isLocalhost = Boolean(
       });
     }
   }
+  
   function registerValidSW(swUrl, config) {
     navigator.serviceWorker
       .register(swUrl)
@@ -78,37 +93,43 @@ const isLocalhost = Boolean(
           };
         };
       })
-      function checkValidServiceWorker(swUrl, config) {
-        // Check if the service worker can be found. If it can't reload the page.
-        fetch(swUrl)
-          .then(response => {
-            // Ensure service worker exists, and that we really are getting a JS file.
-            const contentType = response.headers.get('content-type');
-            if (
-              response.status === 404 ||
-              (contentType != null && contentType.indexOf('javascript') === -1)
-            ) {
-              // No service worker found. Probably a different app. Reload the page.
-              navigator.serviceWorker.ready.then(registration => {
-                registration.unregister().then(() => {
-                  window.location.reload();
-                });
-              });
-            } else {
-              // Service worker found. Proceed as normal.
-              registerValidSW(swUrl, config);
-            }
-          })
-          .catch(() => {
-            console.log(
-              'No internet connection found. App is running in offline mode.'
-            );
-          });
-      }
-      export function unregister() {
-        if ('serviceWorker' in navigator) {
+      .catch(error => {
+        console.error('Error during service worker registration:', error);
+      });
+  }
+  
+  function checkValidServiceWorker(swUrl, config) {
+    // Check if the service worker can be found. If it can't reload the page.
+    fetch(swUrl)
+      .then(response => {
+        // Ensure service worker exists, and that we really are getting a JS file.
+        const contentType = response.headers.get('content-type');
+        if (
+          response.status === 404 ||
+          (contentType != null && contentType.indexOf('javascript') === -1)
+        ) {
+          // No service worker found. Probably a different app. Reload the page.
           navigator.serviceWorker.ready.then(registration => {
-            registration.unregister();
+            registration.unregister().then(() => {
+              window.location.reload();
+            });
           });
+        } else {
+          // Service worker found. Proceed as normal.
+          registerValidSW(swUrl, config);
         }
-      }
+      })
+      .catch(() => {
+        console.log(
+          'No internet connection found. App is running in offline mode.'
+        );
+      });
+  }
+  
+  export function unregister() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.unregister();
+      });
+    }
+  }
